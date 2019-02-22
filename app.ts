6b3ea417ce3c3ec27929ced8c072dbecb7e221f5
SuuQi <646382806@qq.com>
2019-02-22 10:46:53 +0800

    添加追书接口代理

--- app.ts
+++ app.ts
@@ -7,7 +7,28 @@
  */
 
 import { EggApplication } from 'egg';
+import * as proxy from 'koa-proxy';
 
 export default (app: EggApplication) => {
-    console.log(app);
+    app.beforeStart(() => {
+        /** 代理追书接口 */
+        app.use(proxy({
+            host: 'http://api.zhuishushenqi.com',
+            match: /^\/api\/book-api\//,
+            jar: true,
+            map: (path: string) => path.replace(/^\/api\/book-api/, '')
+        }));
+        app.use(proxy({
+            host: 'http://statics.zhuishushenqi.com',
+            match: /^\/api\/book-statics\//,
+            jar: true,
+            map: (path: string) => path.replace(/^\/api\/book-statics/, '')
+        }));
+        app.use(proxy({
+            host: 'http://chapter2.zhuishushenqi.com',
+            match: /^\/api\/book-chapter2\//,
+            jar: true,
+            map: (path: string) => path.replace(/^\/api\/book-chapter2/, '')
+        }));
+    })
 };
