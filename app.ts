c41e5817f7a1f61e3653c1a747fb5d2033e826c6
SuuQi <646382806@qq.com>
2019-02-24 14:31:00 +0800

    接口

--- app.ts
+++ app.ts
@@ -2,11 +2,12 @@
  * @Author: hzsuqin 
  * @Date: 2018-10-04 10:27:14 
  * @Last Modified by: hzsuqin
- * @Last Modified time: 2019-02-24 14:27:20
+ * @Last Modified time: 2019-02-24 14:30:53
  * @description: 服务端入口，处理egg额外操作
  */
 
 import { EggApplication } from 'egg';
 
 export default (app: EggApplication) => {
+    console.log(app);
 };
