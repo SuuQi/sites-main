5b3291cef421cc7f1765854eaa2805c0248eae59
SuuQi <646382806@qq.com>
2019-02-24 14:30:20 +0800

    更新代理接口

--- app.ts
+++ app.ts
@@ -2,33 +2,11 @@
  * @Author: hzsuqin 
  * @Date: 2018-10-04 10:27:14 
  * @Last Modified by: hzsuqin
- * @Last Modified time: 2018-10-04 10:28:11
+ * @Last Modified time: 2019-02-24 14:27:20
  * @description: 服务端入口，处理egg额外操作
  */
 
 import { EggApplication } from 'egg';
-import * as proxy from 'koa-proxy';
 
 export default (app: EggApplication) => {
-    app.beforeStart(() => {
-        /** 代理追书接口 */
-        app.use(proxy({
-            host: 'http://api.zhuishushenqi.com',
-            match: /^\/api\/book-api\//,
-            jar: true,
-            map: (path: string) => path.replace(/^\/api\/book-api/, '')
-        }));
-        app.use(proxy({
-            host: 'http://statics.zhuishushenqi.com',
-            match: /^\/api\/book-statics\//,
-            jar: true,
-            map: (path: string) => path.replace(/^\/api\/book-statics/, '')
-        }));
-        app.use(proxy({
-            host: 'http://chapter2.zhuishushenqi.com',
-            match: /^\/api\/book-chapter2\//,
-            jar: true,
-            map: (path: string) => path.replace(/^\/api\/book-chapter2/, '')
-        }));
-    })
 };
