632a06cefe803951b04fcd0a1d568ce8afa82408
SuuQi <646382806@qq.com>
2019-03-01 22:16:01 +0800

    删除typings

--- typings/app/extend/request.d.ts
+++
@@ -1,9 +0,0 @@
-// This file was auto created by egg-ts-helper
-// Do not modify this file!!!!!!!!!
-
-import 'egg'; // Make sure ts to import egg declaration at first
-import ExtendRequest from '../../../app/extend/request';
-declare module 'egg' {
-  type ExtendRequestType = typeof ExtendRequest;
-  interface Request extends ExtendRequestType { }
-}
--- typings/config/plugin.d.ts
+++
@@ -1,6 +0,0 @@
-// This file is created by egg-ts-helper
-// Do not modify this file!!!!!!!!!
-
-import 'egg';
-import 'egg-view-ejs';
-import 'egg-mongoose';
