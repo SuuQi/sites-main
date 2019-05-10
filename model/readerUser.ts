d36ac4d0c4f4e29e7265fc6782b6a9528205e402
苏秦 <15957136119@163.com>
2019-05-10 19:04:22 +0800

    update缩略图

---
+++ model/readerUser.ts
@@ -0,0 +1,26 @@
+/*
+ * @Author: hzsuqin 
+ * @Last Modified by: hzsuqin
+ * @description: reader模块Model文件
+ */
+
+export default (app: any) => {
+    const mongoose = app.mongoose;
+    const HelloSchema = new mongoose.Schema({
+        openid: { type: 'string', required: true },
+        sessionId: { type: 'string', required: true },
+        books: [
+            {
+                title: { type: 'string', required: true },
+                id: { type: 'string', required: true },
+                lastIndex: { type: 'number', default: 0 },
+                book: { type: 'string', required: true },
+                chaptersUpdated: { type: 'string', default: '' },
+                updated: { type: 'string', default: '' },
+                cover: { type: 'string', required: true }
+            }
+        ]
+    });
+
+    return mongoose.model('ReaderUser', HelloSchema);
+}
