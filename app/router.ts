faca98f83e0f40ef1880e5a0b007f61bc29488c1
SuuQi <646382806@qq.com>
2019-05-12 12:13:15 +0800

    添加gzip

--- app/router.ts
+++ app/router.ts
@@ -12,19 +12,19 @@ export default (app: Application) => {
     /** reader代理接口 */
     router.get(/\/api\/book-api\/.*/, async (ctx, next) => {
         await next();
-        ctx.body = await ctx.curl('http://api.zhuishushenqi.com' + ctx.request.url.replace(/^\/api\/book-api/, ''), { dataType: 'json' });
+        ctx.body = await ctx.curl('http://api.zhuishushenqi.com' + ctx.request.url.replace(/^\/api\/book-api/, ''), { dataType: 'json', gzip: true });
         ctx.body = ctx.body.data;
     });
     /** reader代理章节 */
     router.get(/\/api\/book-chapter2\/.*/, async (ctx, next) => {
         await next();
-        ctx.body = await ctx.curl('http://chapter2.zhuishushenqi.com' + ctx.request.url.replace(/^\/api\/book-chapter2/, ''), { dataType: 'json' });
+        ctx.body = await ctx.curl('http://chapter2.zhuishushenqi.com' + ctx.request.url.replace(/^\/api\/book-chapter2/, ''), { dataType: 'json', gzip: true });
         ctx.body = ctx.body.data;
     });
     /** reader代理静态资源 */
     router.get(/\/api\/book-statics\/.*/, async (ctx, next) => {
         await next();
-        ctx.body = await ctx.curl('http://statics.zhuishushenqi.com' + ctx.request.url.replace(/^\/api\/book-statics/, ''));
+        ctx.body = await ctx.curl('http://statics.zhuishushenqi.com' + ctx.request.url.replace(/^\/api\/book-statics/, ''), { gzip: true });
         ctx.body = ctx.body.data;
     });
 
